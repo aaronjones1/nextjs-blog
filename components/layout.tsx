339c838b8edeb657fad59b06616091e9857780b2
aaronjones1 <ajones@cycominc.com>
2023-02-11 13:25:21 -0500

    Add Storybook

--- components/layout.tsx
+++ components/layout.tsx
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Aaron';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'Muze Testing';
 
 export default function Layout({
   children,
