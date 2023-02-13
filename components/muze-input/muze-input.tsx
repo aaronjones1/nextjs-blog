03902ca8b006f2c0b8c89eda1151ad6c54490e61
aaronjones1 <ajones@cycominc.com>
2023-02-12 22:43:26 -0500

    MuzeInput styling update

--- components/muze-input/muze-input.tsx
+++ components/muze-input/muze-input.tsx
@@ -11,7 +11,7 @@ export default function MuzeInput(props: MuzeInputProps) {
       </label>
       <input
         id='mzinput'
-        className='rounded-full px-2.5 py-1 outline-none border-2 border-transparent focus:border-neutral-700 ring-neutral-50 bg-neutral-800 text-neutral-50'
+        className='rounded-full px-2.5 py-1 outline-none border-2 border-transparent focus:border-neutral-700 ring-neutral-50 bg-neutral-800 text-neutral-50 shadow-md shadow-neutral-900'
         placeholder='Muze Input'
       />
       {props.helperMessage ? (
